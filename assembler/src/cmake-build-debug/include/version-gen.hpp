c9315ab20b15c53aad184bb50e7d5b98e350ac63
1dayac <meleshko.dmitrii@gmail.com>
2019-08-16 12:10:00 -0400

    Update cmakefile

--- assembler/src/cmake-build-debug/include/version-gen.hpp
+++ assembler/src/cmake-build-debug/include/version-gen.hpp
@@ -2,6 +2,6 @@
 #define __SPADES_VERSION_HPP__
 
 #define SPADES_GIT_REFSPEC "refs/heads/master"
-#define SPADES_GIT_SHA1 "927876a13132eca36bd6a6a50732e16f0254b8b5"
+#define SPADES_GIT_SHA1 "2cac6c594712f448ac02f79ed463472b529499cf"
 
 #endif // __SPADES_VERSION_HPP__
