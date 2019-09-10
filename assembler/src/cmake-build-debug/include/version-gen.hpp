ca9341e5bb891f86efc72d8ffdb811f9fc72841c
1dayac <meleshko.dmitrii@gmail.com>
2019-09-10 05:08:46 -0400

    Try to work with scaffolds

--- assembler/src/cmake-build-debug/include/version-gen.hpp
+++ assembler/src/cmake-build-debug/include/version-gen.hpp
@@ -2,6 +2,6 @@
 #define __SPADES_VERSION_HPP__
 
 #define SPADES_GIT_REFSPEC "refs/heads/master"
-#define SPADES_GIT_SHA1 "4fc17aec5a56f2284ed11a9318f6f707ba2bc7e2"
+#define SPADES_GIT_SHA1 "319df57160405581df653a795da039e9d18b0f03"
 
 #endif // __SPADES_VERSION_HPP__
