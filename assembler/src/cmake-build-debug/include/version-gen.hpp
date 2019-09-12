732cf1235523a364b5890424fb0c66dccf61b401
dmm2017 <meleshko.dmitrii@gmail.com>
2019-09-12 09:31:55 +0300

    Before rr

--- assembler/src/cmake-build-debug/include/version-gen.hpp
+++ assembler/src/cmake-build-debug/include/version-gen.hpp
@@ -2,6 +2,6 @@
 #define __SPADES_VERSION_HPP__
 
 #define SPADES_GIT_REFSPEC "refs/heads/master"
-#define SPADES_GIT_SHA1 "ca9341e5bb891f86efc72d8ffdb811f9fc72841c"
+#define SPADES_GIT_SHA1 "97ef7cbc279acd1095b1cd40fbe8d26c79498bd6"
 
 #endif // __SPADES_VERSION_HPP__
