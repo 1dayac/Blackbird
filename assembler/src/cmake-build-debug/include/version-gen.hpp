4b55c3f8ec9269f6d1f1eae70b20846f2299a398
dmm2017 <meleshko.dmitrii@gmail.com>
2019-10-04 17:29:37 +0300

    Temp commit simulated

--- assembler/src/cmake-build-debug/include/version-gen.hpp
+++ assembler/src/cmake-build-debug/include/version-gen.hpp
@@ -2,6 +2,6 @@
 #define __SPADES_VERSION_HPP__
 
 #define SPADES_GIT_REFSPEC "refs/heads/master"
-#define SPADES_GIT_SHA1 "a32985cb51cdbcddb2fa79ab49221be755de80ff"
+#define SPADES_GIT_SHA1 "5a064ac85eb06808fcbc394661fa479dab4914e6"
 
 #endif // __SPADES_VERSION_HPP__
