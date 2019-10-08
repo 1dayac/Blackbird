123b7063e30d4478f989ef31edebf890a71746fe
dmm2017 <meleshko.dmitrii@gmail.com>
2019-10-08 17:47:47 +0300

    Try new definition

--- assembler/src/projects/blackbird/utils/pipeline.h
+++ assembler/src/projects/blackbird/utils/pipeline.h
@@ -322,16 +322,14 @@ public:
     }
 
 private:
-    template <class K, class V,
-            class Hash  = phmap::container_internal::hash_default_hash<K>,
-            class Eq    = phmap::container_internal::hash_default_eq<K>,
-            class Alloc = phmap::container_internal::Allocator<
-                    phmap::container_internal::Pair<const K, V>>, // alias for std::allocator
-            size_t N    = 4,                  // 2**N submaps
-            class Mutex = phmap::NullMutex>   // use std::mutex to enable internal locks
-    class parallel_flat_hash_map;
 
-    parallel_flat_hash_map<std::string, std::vector<io::SingleRead>> map_of_bad_reads_;
+    phmap::parallel_flat_hash_map<std::string, std::vector<io::SingleRead>,
+            phmap::container_internal::hash_default_hash<std::string>,
+            phmap::container_internal::hash_default_eq<std::string>,
+            phmap::container_internal::Allocator<
+            phmap::container_internal::Pair<const std::string, std::vector<io::SingleRead>>>,
+            4, phmap::NullMutex> map_of_bad_reads_;
+
     VCFWriter writer_;
     VCFWriter writer_small_;
     std::vector<Insertion> vector_of_small_ins_;
