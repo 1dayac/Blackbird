ed5ed00767b8c329114d06a5207871034d983567
1dayac <meleshko.dmitrii@gmail.com>
2020-01-06 20:32:01 -0500

    Add inversions

--- assembler/src/cmake-build-debug/include/version-gen.hpp
+++ assembler/src/cmake-build-debug/include/version-gen.hpp
@@ -2,6 +2,6 @@
 #define __SPADES_VERSION_HPP__
 
 #define SPADES_GIT_REFSPEC "refs/heads/debug_multithread"
-#define SPADES_GIT_SHA1 "ae024080c82f42512577ce3cb85c5c675833557a"
+#define SPADES_GIT_SHA1 "a0ccfae69468f4355f393f7b628bcc89408dadd4"
 
 #endif // __SPADES_VERSION_HPP__
