600dd5f3f7fe5e184a9abfd562a9b949bb825cea
dmm2017 <meleshko.dmitrii@gmail.com>
2021-03-22 19:24:09 +0300

    Diff libs

--- assembler/ext/src/unimap2/cmake_install.cmake
+++ assembler/ext/src/unimap2/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/dima/Desktop/Blackbird2/assembler/ext/src/minimap2
+# Install script for directory: /home/dmm2017/Desktop/Blackbird/assembler/ext/src/unimap2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,6 +27,16 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -35,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/dima/Desktop/Blackbird2/assembler/ext/src/minimap2/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/dmm2017/Desktop/Blackbird/assembler/ext/src/unimap2/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
