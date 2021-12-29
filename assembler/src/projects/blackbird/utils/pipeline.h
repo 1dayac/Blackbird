193cf9eeb975d2cca73e142ab7a46cbe6bbaf5f0
1dayac <meleshko.dmitrii@gmail.com>
2021-12-29 18:19:45 -0500

    Remove

--- assembler/src/projects/blackbird/utils/pipeline.h
+++ assembler/src/projects/blackbird/utils/pipeline.h
@@ -205,7 +205,7 @@ public:
             writer.Open(new_bam_name, preliminary_reader.GetConstSamHeader(), preliminary_reader.GetReferenceData());
             long long total = 0;
             while (preliminary_reader.GetNextAlignment(alignment)) {
-                break;
+
                 int tag = 5;
 
                 bool res = alignment.GetTag("AM", tag);
